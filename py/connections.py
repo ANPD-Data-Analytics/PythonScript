dca56362a5f68db9f22389a763b059949b77c8ba
Subedi <manish.subedi@abbott.com>
2023-09-28 17:19:55 -0400

    added new folder

---
+++ py/connections.py
@@ -0,0 +1,29 @@
+import os
+
+sysUser511 =os.getlogin()
+print (sysUser511)
+print (os.getcwd() )
+os.chdir("C:/Users/"+sysUser511+"/OneDrive - Abbott/LoginInfo")
+
+print (os.getcwd() )
+try:
+    with open("CDP_Connection_String.txt", "r") as file:
+        cdp_con = file.read()
+        print(cdp_con)
+except FileNotFoundError:
+    cdp_con = None
+
+try:
+    with open("Shiloh_acc_key.txt", "r") as file:
+        shi_key = file.read()
+        print(shi_key)
+except FileNotFoundError:
+    shi_key = None
+
+try:
+    with open("Shiloh_acc_name.txt", "r") as file:
+        shi_name = file.read()
+        print(shi_name)
+except FileNotFoundError:
+    shi_name = None
+
