484e4b5c8353be64441262d33c15e047e3923fe4
msubediabbott <135062876+msubediabbott@users.noreply.github.com>
2023-10-05 13:52:29 -0400

    Update connections.py

--- py/connections.py
+++ py/connections.py
@@ -4,25 +4,24 @@ sysUser511 =os.getlogin()
 #print (sysUser511)
 #print (os.getcwd() )
 os.chdir("C:/Users/"+sysUser511+"/OneDrive - Abbott/LoginInfo")
-#
-print (os.getcwd() )
-#try:
-#    with open("CDP_Connection_String.txt", "r") as file:
-#        cdp_con = file.read()
-#        print(cdp_con)
-#except FileNotFoundError:
-#    cdp_con = None
-#
-#try:
-#    with open("Shiloh_acc_key.txt", "r") as file:
-#        shi_key = file.read()
-#        print(shi_key)
-#except FileNotFoundError:
-#    shi_key = None
-#
-#try:
-#    with open("Shiloh_acc_name.txt", "r") as file:
-#        shi_name = file.read()
-#        print(shi_name)
-#except FileNotFoundError:
-#    shi_name = None
+wd = os.getcwd() 
+try:
+    with open("CDP_Connection_String.txt", "r") as file:
+        cdp_con = file.read()
+        #print(cdp_con)
+except FileNotFoundError:
+    cdp_con = None
+
+try:
+    with open("Shiloh_acc_key.txt", "r") as file:
+        shi_key = file.read()
+        #print(shi_key)
+except FileNotFoundError:
+    shi_key = None
+
+try:
+    with open("Shiloh_acc_name.txt", "r") as file:
+        shi_name = file.read()
+        #print(shi_name)
+except FileNotFoundError:
+    shi_name = None
