389ff3d6b60e8904f2cc72989b53561e5cf368c2
msubediabbott <135062876+msubediabbott@users.noreply.github.com>
2023-10-06 11:35:06 -0400

    Update file_checker_wm.py

--- py/file_checker_wm.py
+++ py/file_checker_wm.py
@@ -20,9 +20,9 @@ fnStore = 'WMStore_'+str(date.today())+'.csv.gz'
 target_filenames = [fnCal,fnFact,fnItem,fnStore]
 
 #Setting Log File properties
-logging.basicConfig(filename='WMLog_'+str(date.today())+'.log',
+logging.basicConfig(filename='log/WMLog_'+str(date.today())+'.log',
                     format='%(asctime)s %(message)s',
-                    filemode='w')
+                    filemode='a')
 logger.info("Start of the Script")
 while target_filenames:
     # List all files in the source container
@@ -44,7 +44,8 @@ while target_filenames:
             # Remove the file from the list of target filenames
             target_filenames.remove(file_name)
             #Once all 4 files are transferred, delete the files from Source, Walmart's Container
-            
+            time.sleep(180)
+            source_blob_client.delete_blob()
             
     
     if target_filenames:
@@ -52,7 +53,7 @@ while target_filenames:
             # Sleep for a while before checking again
             print("Sunday Run: Not all files found.")
             logger.info(f"No Retries on Sunday. The Sunday job triggers at 6 PM")
-            #send_email()
+            send_email()
             sys.exit(0)
 
         else:
@@ -60,7 +61,7 @@ while target_filenames:
                 i+=1
                 print("Not all files found. Sleeping for 1 hour and retrying...")
                 logger.info(f"Retrying after 1 hour,Not all files found. No of Attempts: {i}. Total Attempts: 3")
-                time.sleep(30)
+                time.sleep(3600)
                 print(i)
                 if i>=5:
                     logger.info("Didn't received all Walmart Files, Sending email to the team to validate and escalate...")
@@ -69,19 +70,16 @@ while target_filenames:
                     sys.exit(0)
     else:
         logger.info("All Source files found and copied to the destination container...")
-        logger.info("Sleeping for 30 sec and creating Trigger file.")
-        time.sleep(600)
+        logger.info("Sleeping for 5 min.")
+        time.sleep(180)
         trigger_file = 'trigger.txt'
         file_content = ''
         #Creating a trigger.txt file 
+        logger.info("Creating Trigger file.")
         blob_trigger_file = destination_container_client.get_blob_client(trigger_file)
         blob_trigger_file.upload_blob(file_content,overwrite=True)
         print("Trigger File Successfully Created in Extract directory.")
         logger.info("Trigger File Successfully Created in Extract directory...")
         logger.info("Program completed successfully, Exiting...")
-        source_blob_client.delete_blob()
         sys.exit(0)
     logger.info("End of the Script")
-
-
-
